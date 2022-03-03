5e234077dadd7a09b6c4c72b390123620d823727
bryanrp <66724556+bryanrp@users.noreply.github.com>
2022-03-03 23:42:28 +0700

    Add level and fix multiple PhysicsObject duplication
    PhysicsObject with a PhysicsObject child will create 4 duplication, for example a PressurePlate in a MovablePlatform

--- Assets/Scripts/PhysicsObject.cs
+++ Assets/Scripts/PhysicsObject.cs
@@ -6,11 +6,14 @@ public class PhysicsObject : MonoBehaviour
 {
     protected PhysicsObject _physicsPrimary;
     protected PhysicsObject _physicsSecondary;
+    [SerializeField] protected bool _duplicateAtStart = true;
     [SerializeField] protected int _physicsType = 0;
 
     // Start is called before the first frame update
     protected void Start()
     {
+        if (!_duplicateAtStart) return;
+
         if (!IsDuplicate())
         {
             if (_physicsType == 1)
@@ -18,19 +21,13 @@ public class PhysicsObject : MonoBehaviour
                 transform.SetParent(null);
                 PhysicsManager.Instance.MoveGameObjectToScene(gameObject, 1);
             }
-
-            _physicsSecondary = Instantiate(gameObject).GetComponent<PhysicsObject>();
+            
+            SetPrimary(Instantiate(gameObject).GetComponent<PhysicsObject>());
             _physicsSecondary.transform.position = transform.position;
-            _physicsSecondary.SetDuplicate(this);
+            _physicsSecondary.SetSecondary(this);
             PhysicsManager.Instance.MoveGameObjectToScene(_physicsSecondary.gameObject, 1 - _physicsType);
-        }
-        else
-        {
-            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-            if (spriteRenderer != null)
-            {
-                spriteRenderer.enabled = false;
-            }
+
+            ConnectChildWithPhysicsObject();
         }
     }
 
@@ -40,14 +37,66 @@ public class PhysicsObject : MonoBehaviour
         
     }
 
+    private void ConnectChildWithPhysicsObject()
+    {
+        List<PhysicsObject> primaryChild = new List<PhysicsObject>();
+        foreach (Transform child in transform) {
+            PhysicsObject childPhysicsObject = child.GetComponent<PhysicsObject>();
+            if (childPhysicsObject != null) primaryChild.Add(childPhysicsObject);
+        }
+
+        List<PhysicsObject> secondaryChild = new List<PhysicsObject>();
+        foreach (Transform child in _physicsSecondary.transform)
+        {
+            PhysicsObject childPhysicsObject = child.GetComponent<PhysicsObject>();
+            if (childPhysicsObject != null) secondaryChild.Add(childPhysicsObject);
+        }
+
+        if (primaryChild.Count != secondaryChild.Count) Debug.LogError("primaryChild.Count differs from secondaryChild.Count: " + primaryChild.Count + " vs " + secondaryChild.Count);
+        for (int i = 0; i < primaryChild.Count; i++)
+        {
+            bool found = false;
+            for (int j = 0; j < secondaryChild.Count; j++)
+            {
+                if (primaryChild[i].name == secondaryChild[j].name)
+                {
+                    primaryChild[i].SetPrimary(secondaryChild[j]);
+                    secondaryChild[j].SetSecondary(primaryChild[i]);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) Debug.LogError("Child with name: " + primaryChild[i].name + " does not have a pair");
+        }
+    }
+
     protected bool IsDuplicate()
     {
         return _physicsPrimary != null;
     }
 
-    public void SetDuplicate(PhysicsObject primary)
+    public void SetSecondary(PhysicsObject primary)
     {
+        if (_physicsPrimary != null) Debug.LogWarning("Reassigning physicsObject to a PhysicsObject that already have a _physicsPrimary");
         _physicsPrimary = primary;
+
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+    public void SetPrimary(PhysicsObject secondary)
+    {
+        if (_physicsSecondary != null) Debug.LogWarning("Reassigning physicsObject to a PhysicsObject that already have a _physicsSecondary");
+        _physicsSecondary = secondary;
+        
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = true;
+        }
     }
 
     public void SetActive(bool value)
